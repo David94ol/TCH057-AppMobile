4f6650e2dbe45de1a0a59a274aa864ac3c92c064
Zakben23 <155661880+Zakben23@users.noreply.github.com>
2024-03-27 13:37:46 -0400

    Page principale locataire(Par Zakaria)

--- app/src/main/java/com/david/appprojet/PageLogIn_Main.java
+++ app/src/main/java/com/david/appprojet/PageLogIn_Main.java
@@ -12,7 +12,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PageLogIn extends AppCompatActivity{
+public class PageLogIn_Main extends AppCompatActivity{
 
     //Composants de la page qui sont manipulés
     private EditText usernameId, passwordId;
@@ -34,7 +34,7 @@ public class PageLogIn extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 //On commence l'activité d'inscription
-                Intent inscription = new Intent(PageLogIn.this, PageInscription.class);
+                Intent inscription = new Intent(PageLogIn_Main.this, PageInscription.class);
                 startActivity(inscription);
                 finish();
             }
@@ -44,7 +44,7 @@ public class PageLogIn extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 //On commence l'activité de connexion TODO : A MODIFIER SEULEMENT POUR TEST
-                Intent connexion = new Intent(PageLogIn.this, PageAfficheAppartment.class);
+                Intent connexion = new Intent(PageLogIn_Main.this, PagePrincipaleLocataire.class);
                 startActivity(connexion);
                 finish();
             }
